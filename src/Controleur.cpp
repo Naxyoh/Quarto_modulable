6db435bb2558a575102f2e50d22bd8a49c86493d
Naxyoh <yoan.smit@gmail.com>
2014-12-09 11:52:28 +0100

    Controleur SFML
    Ajout de la fonction de selection de piece pour le controleur sfml

--- src/Controleur.cpp
+++ src/Controleur.cpp
@@ -29,8 +29,44 @@ void ControleurConsole::jouerPiece(board &myBoard, Piece pieceAJouer)
     myBoard.eraseFromJouable(pos-1);
 }
 
-//Piece ControleurSFML::selectionnerPiece(board &monPlateau)
-//{
-//    int position = -1;
-//
-//}
+Piece ControleurSFML::selectionnerPiece(board &monPlateau)
+{
+    int position = -1;
+    sf::Event event;
+    if (event.type == sf::Event::MouseButtonPressed){
+        sf::Mouse::Button button = event.mouseButton.button;
+        if (button == sf::Mouse::Left)
+        {
+            if (event.mouseButton.y < 70 && event.mouseButton.y > 10)
+            {
+                if (event.mouseButton.x < 170 && event.mouseButton.x > 100) position = 0;
+                else if (event.mouseButton.x < 240 && event.mouseButton.x > 170) position = 1;
+                else if (event.mouseButton.x < 310 && event.mouseButton.x> 240) position = 2;
+                else if (event.mouseButton.x < 380 && event.mouseButton.x > 310) position = 3;
+                else if (event.mouseButton.x < 450 && event.mouseButton.x > 380) position = 4;
+                else if (event.mouseButton.x < 520 && event.mouseButton.x > 450) position = 5;
+                else if (event.mouseButton.x < 590 && event.mouseButton.x > 520) position = 6;
+                else if (event.mouseButton.x < 660 && event.mouseButton.x > 590) position = 7;
+            }
+            else if (event.mouseButton.y < 140 && event.mouseButton.y > 80)
+            {
+                if (event.mouseButton.x < 170 && event.mouseButton.x > 100) position = 8;
+                else if (event.mouseButton.x < 240 && event.mouseButton.x > 170) position = 9;
+                else if (event.mouseButton.x < 310 && event.mouseButton.x> 240) position = 10;
+                else if (event.mouseButton.x < 380 && event.mouseButton.x > 310) position = 11;
+                else if (event.mouseButton.x < 450 && event.mouseButton.x > 380) position = 12;
+                else if (event.mouseButton.x < 520 && event.mouseButton.x > 450) position = 13;
+                else if (event.mouseButton.x < 590 && event.mouseButton.x > 520) position = 14;
+                else if (event.mouseButton.x < 660 && event.mouseButton.x > 590) position = 15;
+            }
+        }
+    }
+	Piece pieceSelectionne = monPlateau.getListePieceJouable()[position];
+	return pieceSelectionne;
+}
+
+
+void ControleurSFML::jouerPiece(board& myBoard, Piece pieceAJouer)
+{
+
+}
